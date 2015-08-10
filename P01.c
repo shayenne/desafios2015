2d5d890ed1989af1d29f115e498cfb0a648d27ee
mac0327 27 <mac0327.27@metralha.cec.ime.usp.br>
2015-08-10 15:58:26 -0300

    Resolvido 1 e 11

---
+++ P01.c
@@ -0,0 +1,42 @@
+/* Minimum Difficulty*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+  int *A;
+  int i, min, max;
+  int n, x;
+
+  x = 0;
+  min = 1000;
+  max = 0;
+  
+  scanf("%d", &n);
+   
+  A = (int *) malloc (n * sizeof(int));
+  
+  for (i = 0; i < n; i++)
+    scanf("%d", &A[i]);
+
+  for (i = 0; i < n-1; i++){
+    x = A[i+1]-A[i];
+    if (x > max)
+      max = x;
+
+    if (i < n-2){
+      x = A[i+2]-A[i];
+      if (x < min){
+	min = x;
+      }
+    }
+  }
+  
+  if (min < max)
+    min = max;
+
+  printf("%d", min);
+
+  free(A);
+
+  return 0;
+}
---
+++ P11.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+  long **A, *s;
+  int n, f;
+  int i, j, k;
+  long sum, val;
+  long  x;
+  
+  scanf("%d", &n);
+
+  s = (long *) malloc(n*sizeof(long));
+  
+  k = 0;
+
+  while (k < n) {
+    scanf("%d", &f);
+    
+    sum = 0;
+
+    for (i = 0; i < f; i++) {
+      for (j = 0, val = 1; j < 3; j++) {
+	scanf("%d", &x);
+	if (j != 1)
+	  val *= x;
+      }
+      sum += val;
+    }
+    
+    s[k] = sum;
+    k++;
+  }
+
+  for (i = 0; i < n; i++){
+    printf("%li\n", s[i]);
+  }
+  free(s);
+  
+  return 0;
+}
