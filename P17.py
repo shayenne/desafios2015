351758d3cd0000f1a0d206d5e595515a9f174c45
Shayenne Luz Moura <shayenne.moura@usp.br>
2015-08-18 16:04:44 -0300

    Tentativa do 17

--- P17.py
+++ P17.py
@@ -9,20 +9,49 @@ for i in range(n):
     x[1] = int(x[1])
     paint.append(x)
 
+def busca_binaria_ini(x, v):
+    e = 0
+    d = len(v)-1
+    while e <= d:
+        m = (e+d)/2
+        if v[m][0] <= x and v[m][1] > x:
+            return m
+        if v[m][1] < x:
+            e = m + 1
+        else:
+            d = m - 1
+    return 0
 
+def busca_binaria_fim(x, v):
+    e = 0
+    d = len(v)-1
+    while e <= d:
+        m = (e+d)/2
+        if v[m][1] >= x and v[m][0] < x:
+            return m
+        if v[m][0] < x:
+            e = m + 1
+        else:
+            d = m - 1
+    return 0
 
 faixas = [[0, 1000000000, 'w']]
 
 numfaixas = 1
 
 for i in range(n):
+    
+    ant = busca_binaria_ini(paint[i][0], faixas)
+    pos = busca_binaria_fim(paint[i][1], faixas)
+    """
     ant = 0
     while faixas[ant][1] < paint[i][0]:
         ant += 1
+    
     pos = ant
     while faixas[pos][1] < paint[i][1]:
         pos += 1
-    
+    """
 
     # Olha para a nova pintura e para onde ficam o inicio e o fim
     # Ambos os extremos estao em uma unica faixa
@@ -59,7 +88,7 @@ for i in range(n):
                 faixas[ant][1] = paint[i][0]
                 faixas[pos][0] = paint[i][0]
                 del faixas[ant+1:pos]
-               
+
 
 fmax = [0, 0, 0]
 for faixa in faixas:
