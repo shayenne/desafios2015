04849c85e3b1c7beb8e00fa5c74a8f91e96216b2
Shayenne Luz Moura <shayenne.moura@usp.br>
2015-09-04 14:38:09 -0300

    Tentativa 27

--- P27.py
+++ P27.py
@@ -1,81 +1,39 @@
 #!/usr/bin/env python
 
-class BinaryTree():
-
-    def __init__(self,rootid):
-      self.left = None
-      self.right = None
-      self.rootid = rootid
-
-    def getLeftChild(self):
-        return self.left
-    def getRightChild(self):
-        return self.right
-    def setNodeValue(self,value):
-        self.rootid = value
-    def getNodeValue(self):
-        return self.rootid
-
-    def insertRight(self,newNode):
-        if self.right == None:
-            self.right = BinaryTree(newNode)
+def busca_binaria(x, v):
+    print v
+    e = 0
+    d = len(v)-1
+    while e <= d:
+        m = int((e+d)/2)
+        if x >= v[m][0] and x < v[m][1]:
+            return m
+        if x >= v[m][1]:
+            e = m + 1
         else:
-            tree = BinaryTree(newNode)
-            tree.right = self.right
-            self.right = tree
+            d = m - 1
+    print "Erro ini"
 
-    def insertLeft(self,newNode):
-        if self.left == None:
-            self.left = BinaryTree(newNode)
-        else:
-            tree = BinaryTree(newNode)
-            self.left = tree
-            tree.left = self.left
-
-
-def printTree(tree):
-	if tree != None:
-		printTree(tree.getLeftChild())
-		print(tree.getNodeValue())
-		printTree(tree.getRightChild())
-
-
-
-# test tree
-
-def testTree():
-    myTree = BinaryTree("Maud")
-    myTree.insertLeft("Bob")
-    myTree.insertRight("Tony")
-    myTree.insertRight("Steven")
-    printTree(myTree)	
-
-#testTree()
 n = int(raw_input())
 
-first = map(int, raw_input().split())
-tree = BinaryTree(first)
+cnt = 0
+intervalo = [[0, 32000]]
+grau = [0]*n
 
-for i in range(n-1):
-	[x, y] = map(int, raw_input().split())
-	parent = tree
-	val = parent.getNodeValue()
-	child = parent
-	
-	while child != None and (val[0] < x or val[1] < y): 
-		tmp = child
-		child = parent.getLeftChild()
-		parent = tmp
-		val = parent.getNodeValue()
-	"""	
-	if val[0] >= x and val[1] >= y:
-		parent.insertRight([x, y])
-	else:
-		parent.insertLeft([x, y])
-	"""
-	if child == None:
-		parent.insertLeft([x, y])
-	else:
-		parent.insertRight
-	
-printTree(tree)
+x, y = map(int, raw_input().split())
+print x, y
+for i in range(n):
+    lin = y
+    
+    while lin == y:
+        new = busca_binaria(x, intervalo)
+        grau[new] += 1
+        
+        intervalo.insert(new+1, [x, intervalo[new][1]])
+        intervalo[new][1] = x
+
+        x, y = map(int, raw_input().split())
+        
+    cnt += 1
+
+print grau
