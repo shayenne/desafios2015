48268ea1e0345e4cff409a1240bce1e46af489da
Shayenne Luz Moura <shayenne.moura@usp.br>
2015-09-17 09:58:42 -0300

    Tentativa 4

--- 2AVAL/P04.cpp
+++ 2AVAL/P04.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
-int value(n, i, j, tab) {
-  int numbers[10] = {0}*10;
+void imprimeSolucao(int n, int tab[9][9]) {
+  for (int i = 0; i < n*n; i++) {
+    for (int j = 0; j < n*n; j++)
+      cout << tab[i][j] << " ";
+    cout << endl;
+  }
+}
+
+int value(int n, int i, int j, int tab[9][9]) {
+  int numbers[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int k, l;
+  for (k = 0; k < n; k++) {
+    for (l = 1; l < n; l++)
+      if (!tab[k][l]){
+	i = k;
+	j = l;
+	break;
+      }
+    if (i == k && j == l)
+      break;
+  }
   
   for (int k = 0; k < n*n; k++) {
     numbers[tab[i][k]] += 1;
@@ -13,9 +32,12 @@ int value(n, i, j, tab) {
     for (int l = 1; l < n; l++)
     numbers[tab[k][l]] += 1;
 
-  for (int k = 0; k < n; k++)
-    if (!numbers[k])
-      return k;
+  for (int k = 0; k < n*n; k++)
+    if (!numbers[k]){
+      tab[i][j] = k;
+      if (value(n, 0, 0, tab))
+	return 1;	
+    }
   return 0;
 }
 
@@ -29,21 +51,11 @@ int main() {
     for (int j = 0; j < n*n; j++)
       cin >> sudoku[i][j];
 
-  for (int i = 0; i < n*n; i++) {
-    for (int j = 0; j < n*n; j++)
-      if (!sudoku[i][j])
-	if value(n, i, j, sudoku)
-      cout << sudoku[i][j] << " ";
-    cout << endl;
-  }
-
-  for (int i = 0; i < n*n; i++) {
-    for (int j = 0; j < n*n; j++)
-      cout << sudoku[i][j] << " ";
-    cout << endl;
-  }
   
-  cout << n << endl;
+  if (value(n, 0, 0, sudoku))
+     imprimeSolucao(n, sudoku);
+  else
+    cout << "NO SOLUTION" << endl;	
   
   return 0;
 }
