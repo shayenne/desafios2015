2bbab3a19515ae7df373a1f25225e59a1eb87feb
Shayenne Luz Moura <shayenne.moura@usp.br>
2015-09-17 09:38:37 -0300

    2AVAL: 3 resolvido

--- 2AVAL/P03.py
+++ 2AVAL/P03.py
@@ -1,3 +1,4 @@
+# Shayenne Moura
 #!/usr/bin/env python
 
 import sys
@@ -7,31 +8,39 @@ def ContaPossiveis(i, j, tab):
     ki = [2, -2,  2, -2, 1, -1,  1, -1]
     kj = [1,  1, -1, -1, 2,  2, -2, -2]
     cnt = 0
-    print i, j
+
     for l in xrange(8):
         if i+ki[l] >= 0 and i+ki[l] < len(tab) and j+kj[l] >= 0 and j+kj[l] < len(tab) and not tab[i+ki[l]][j+kj[l]]:
             cnt += 1
 
     return cnt
 
-def ResolvePara(i, j, tab):
+def ResolvePara(i, j, tab, qtd):
     ki = [2, -2,  2, -2, 1, -1,  1, -1]
     kj = [1,  1, -1, -1, 2,  2, -2, -2]
+    letra = ["a", "b", "c", "d", "e", "f", "g", "h"]
     way = []
     for k in xrange(8):
         if i+ki[k] >= 0 and i+ki[k] < len(tab) and j+kj[k] >= 0 and j+kj[k] < len(tab) and not tab[i+ki[k]][j+kj[k]]:
-            way.append([i+ki[k], j+kj[k], ContaPossiveis(i+ki[k], j+kj[k], tab)])
-    print way
-    
+            way.append([i+ki[k], j+kj[k], ContaPossiveis(i+ki[k], j+kj[k], tab)])    
     
     way.sort(key=lambda tup: tup[2])
     if len(way) == 0:
-        return False
+        if qtd == n*n:
+            return True
+        else:
+            return False
+        
     k = 0
     while k < len(way):
         tab[way[k][0]][way[k][1]] = 1
-        ResolvePara(way[k][0], way[k][1], tab)
-        return 
+        if ResolvePara(way[k][0], way[k][1], tab, qtd+1):
+            print "{}{}".format(letra[way[k][0]], way[k][1]+1)
+            return True
+        else:
+            tab[way[k][0]][way[k][1]] = 0
+            k += 1
+
 
     return False
 
@@ -45,9 +54,8 @@ for i in xrange(n):
 
 
 tab[0][0] = 1
-print tab
 
-if ResolvePara(0, 0, tab):
-    print "SIM"
+if ResolvePara(0, 0, tab, 1):
+    print "a{}".format(tab[0][0])
 else:
-    print "NAO"
+    print "IMPOSSIBLE"
