8abefae0e864fdbde42a050f6053c42e2e94fdfa
Shayenne Luz Moura <shayenne.moura@usp.br>
2015-09-18 08:36:15 -0300

    2: P07 terminado

--- 2AVAL/P07.py
+++ 2AVAL/P07.py
@@ -12,15 +12,28 @@ n = int(raw_input())
 
 cnt = 0
 husbands = []
-for i in xrange(m):
+for i in xrange(n):
     x = raw_input().split()
-    
-    if x[2] in products and products[x[2]] >= x[0]:
-        products[x[2]] -= x[0]
-        if products[x[2]] == 0:
-            del products[x[2]]
-    else:
-        husbands.append([x[2], int(x[0])])
-    
+    husbands.append([x[2], int(x[0])])
 
-print products
+husbands.append(["Petr", 0])
+
+x = husbands[0]
+cnt = 0
+while husbands[0][0] != "Petr":
+
+    del husbands[0]
+    if x[0] in products:
+        if products[x[0]] >= x[1]:
+            products[x[0]] -= x[1]
+            if products[x[0]] == 0:
+                del products[x[0]]
+        else:
+            husbands.insert(1, [x[0], products[x[0]]])
+    
+    cnt += 1
+    x = husbands[0]
+
+    
+print cnt
+
