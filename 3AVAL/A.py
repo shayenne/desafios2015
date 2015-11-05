c5aa1bb12a52eb8e03ac51e26e5ef24c879e2846
Shayenne Moura <shayenne.moura@usp.br>
2015-11-05 16:22:57 -0200

    quase A

--- 3AVAL/A.py
+++ 3AVAL/A.py
@@ -1,15 +1,66 @@
-n, m = int(raw_input().split())
+import sys
 
-array = 0 in range(n)
+n, m = map(int, raw_input().split())
+
+array = ["*" for x in range(n)]
+diff = [0 for x in range(n)]
+
+op = []
+
 
 for i in xrange(m):
     op.append(map(int, raw_input().split()))
 
 
-for i in xrange(m-1, -1, -1):
+for i in xrange(m):
     if op[i][0] == 1:
-        for j in xrange(op[i][1]-1, op[i][2]-1):
-            array[j] -= op[i][3]
+        for j in xrange(op[i][1]-1, op[i][2]):
+            if array[j] != "*":
+                array[j] += op[i][3]
+            diff[j]  += op[i][3]
 
-    if op[i][0] == 2:
+    elif op[i][0] == 2:
+        rigth = False
+        for j in xrange(op[i][1]-1, op[i][2]):
+            if array[j] >= op[i][3] or array[j] == "*":
+                array[j] = op[i][3]
+                rigth = True
+        if not rigth:
+            print "NO"
+            exit(0)
+
+            
+for i in range(n):
+    if array[i] != "*":
+        array[i] -= diff[i]
+    else:
+        array[i] = 0
+
+
         
+test = array[:]
+
+
+
+for i in xrange(m):
+    if op[i][0] == 1:
+        for j in xrange(op[i][1]-1, op[i][2]):
+            test[j] += op[i][3]
+
+    right = False
+    if op[i][0] == 2:
+        for j in xrange(op[i][1]-1, op[i][2]):
+            if test[j] > op[i][3]:
+                print "NO"
+                exit(0)
+            
+            if test[j] == op[i][3]:
+                right = True            
+    
+        if not right:
+            print "NO"
+            exit(0)
+            
+            
+print "YES"
+print " ".join(str(x) for x in array)
